ade96e53010fcce7a1bfc16912b91ec15f977c50
fercfn <nascimentofernandadev@gmail.com>
2024-09-15 13:54:00 -0300

    Adição de comentários

--- DotNet/Models/Pessoa.cs
+++ DotNet/Models/Pessoa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DotNet.Models
+namespace DotNet.Models //Caminho do projeto
 {
     public class Pessoa // Class name starts with capital letters
     {
