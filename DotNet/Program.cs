43556295d3c8536a918ad815009527ebb7ad86c3
fercfn <nascimentofernandadev@gmail.com>
2024-09-15 13:50:28 -0300

    Classe vogal/Class vowel

--- DotNet/Program.cs
+++ DotNet/Program.cs
@@ -6,21 +6,21 @@
 // person1.Present();
 
 
-using DotNet.Models;
-Stock stockcontrol = new Stock();
+// using DotNet.Models;
+// Stock stockcontrol = new Stock();
 
-stockcontrol.QuantityAvaliable = 30;
-stockcontrol.QuantityWished = 20;
-stockcontrol.Verify();
+// stockcontrol.QuantityAvaliable = 30;
+// stockcontrol.QuantityWished = 20;
+// stockcontrol.Verify();
 
-stockcontrol.QuantityAvaliable = 30;
-stockcontrol.QuantityWished = 30;
-stockcontrol.Verify();
+// stockcontrol.QuantityAvaliable = 30;
+// stockcontrol.QuantityWished = 30;
+// stockcontrol.Verify();
 
-stockcontrol.QuantityAvaliable = 30;
-stockcontrol.QuantityWished = 40;
-stockcontrol.Verify();
+// stockcontrol.QuantityAvaliable = 30;
+// stockcontrol.QuantityWished = 40;
+// stockcontrol.Verify();
 
-stockcontrol.QuantityAvaliable = 30;
-stockcontrol.QuantityWished = 0;
-stockcontrol.Verify();
+// stockcontrol.QuantityAvaliable = 30;
+// stockcontrol.QuantityWished = 0;
+// stockcontrol.Verify();
