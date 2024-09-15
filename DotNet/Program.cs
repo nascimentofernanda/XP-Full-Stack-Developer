c2c7e7123547315563cfebc0a44c5d7cd2324ff8
fercfn <nascimentofernandadev@gmail.com>
2024-09-15 13:36:26 -0300

    Usando a variável booleana/Using the bool

--- DotNet/Program.cs
+++ DotNet/Program.cs
@@ -1,9 +1,9 @@
-﻿using DotNet.Models; //Where the class is located
-Person person1 = new Person();
+﻿// using DotNet.Models; //Where the class is located
+// Person person1 = new Person();
 
-person1.Name = "Fernanda";
-person1.Age = 30;
-person1.Present();
+// person1.Name = "Fernanda";
+// person1.Age = 30;
+// person1.Present();
 
 
 using DotNet.Models;
