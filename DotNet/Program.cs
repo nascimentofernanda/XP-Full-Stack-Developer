68365d6e627781ec5d1198e05adc3df2621efa41
fercfn <nascimentofernandadev@gmail.com>
2024-09-27 17:46:47 -0300

    Alterações no program.cs/changing program.cs

--- DotNet/Program.cs
+++ DotNet/Program.cs
@@ -1,31 +1,31 @@
 ﻿//Person.cs
-using DotNet.Models; //Where the class is located
-Person person1 = new Person();
+// using DotNet.Models; //Where the class is located
+// Person person1 = new Person();
 
-person1.Name = "Fernanda";
-person1.Age = 30;
-person1.Present();
+// person1.Name = "Fernanda";
+// person1.Age = 30;
+// person1.Present();
 
 
 //Stock.cs
-// using DotNet.Models;
-// Stock stockcontrol = new Stock();
+using DotNet.Models;
+Stock stockcontrol = new Stock();
 
-// stockcontrol.QuantityAvaliable = 30;
-// stockcontrol.QuantityWished = 20;
-// stockcontrol.Verify();
+stockcontrol.QuantityAvaliable = 30;
+stockcontrol.QuantityWished = 20;
+stockcontrol.Verify();
 
-// stockcontrol.QuantityAvaliable = 30;
-// stockcontrol.QuantityWished = 30;
-// stockcontrol.Verify();
+stockcontrol.QuantityAvaliable = 30;
+stockcontrol.QuantityWished = 30;
+stockcontrol.Verify();
 
-// stockcontrol.QuantityAvaliable = 30;
-// stockcontrol.QuantityWished = 40;
-// stockcontrol.Verify();
+stockcontrol.QuantityAvaliable = 30;
+stockcontrol.QuantityWished = 40;
+stockcontrol.Verify();
 
-// stockcontrol.QuantityAvaliable = 30;
-// stockcontrol.QuantityWished = 0;
-// stockcontrol.Verify();
+stockcontrol.QuantityAvaliable = 30;
+stockcontrol.QuantityWished = 0;
+stockcontrol.Verify();
 
 
 // Vowel.cs
